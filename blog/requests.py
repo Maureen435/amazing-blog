e83a8258bf62919187806763001b3acc1ce2c722
Maureen435 <mchepkemoi435@gmail.com>
2020-07-20 08:20:55 +0300

    modified the http request

--- blog/requests.py
+++ blog/requests.py
@@ -3,9 +3,6 @@ from .models import Quote
 
 url = "http://quotes.stormconsultancy.co.uk/random.json"
 def get_quote():
-    """
-    Function to consume http request and return a Quote class instance
-    """
     response = requests.get(url).json()
     random_quote = Quote(response.get("author"), response.get("quote"))
     return random_quote
